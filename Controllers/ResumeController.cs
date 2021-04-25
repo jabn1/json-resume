7bc4014a8993db9aab36a3e56109b00d5b4eb402
Jose <jose.berges23@gmail.com>
2021-04-25 16:45:48 -0400

    add POST main sections

--- Controllers/ResumeController.cs
+++ Controllers/ResumeController.cs
@@ -276,6 +276,8 @@ namespace JSON_Resume.Controllers
             return Created($"/resume/work/{work.Company}",null);
         }
 
+    
+
         [HttpPost("volunteer")]
         public IActionResult PostVolunteer([FromBody] Volunteer volunteer)
         {
@@ -300,7 +302,177 @@ namespace JSON_Resume.Controllers
             resume.Volunteer.Add(volunteer);
             return Created($"/resume/work/{volunteer.Organization}",null);
         }
+        
+        [HttpPost("education")]
+        public IActionResult PostEducation([FromBody] Education education)
+        {
+            if(HttpContext.Request.Headers.TryGetValue("Authorization", out StringValues authorization))
+            {
+                if(!Authenticate(authorization,username,password)){
+                    return Unauthorized();
+                }
+            }
+            else
+            {
+                HttpContext.Response.Headers.Add("WWW-Authenticate", "Basic realm=\"Restricted http methods\"");
+                return Unauthorized();
+            }
+            if(resume == null){
+                return NotFound();
+            }
+            if(resume.Education.Any(x => x.Institution == education.Institution)){
+                return Conflict();
+            }
 
+            resume.Education.Add(education);
+            return Created($"/resume/education/{education.Institution}",null);
+        }
+
+        [HttpPost("education")]
+        public IActionResult PostAward([FromBody] Award award)
+        {
+            if(HttpContext.Request.Headers.TryGetValue("Authorization", out StringValues authorization))
+            {
+                if(!Authenticate(authorization,username,password)){
+                    return Unauthorized();
+                }
+            }
+            else
+            {
+                HttpContext.Response.Headers.Add("WWW-Authenticate", "Basic realm=\"Restricted http methods\"");
+                return Unauthorized();
+            }
+            if(resume == null){
+                return NotFound();
+            }
+            if(resume.Awards.Any(x => x.Title == award.Title)){
+                return Conflict();
+            }
+
+            resume.Awards.Add(award);
+            return Created($"/resume/education/{award.Title}",null);
+        }
+        [HttpPost("publications")]
+        public IActionResult PostPublication([FromBody] Publication item)
+        {
+            if(HttpContext.Request.Headers.TryGetValue("Authorization", out StringValues authorization))
+            {
+                if(!Authenticate(authorization,username,password)){
+                    return Unauthorized();
+                }
+            }
+            else
+            {
+                HttpContext.Response.Headers.Add("WWW-Authenticate", "Basic realm=\"Restricted http methods\"");
+                return Unauthorized();
+            }
+            if(resume == null){
+                return NotFound();
+            }
+            if(resume.Publications.Any(x => x.Name == item.Name)){
+                return Conflict();
+            }
+
+            resume.Publications.Add(item);
+            return Created($"/resume/publications/{item.Name}",null);
+        }
+        [HttpPost("skills")]
+        public IActionResult PostSkill([FromBody] Skill item)
+        {
+            if(HttpContext.Request.Headers.TryGetValue("Authorization", out StringValues authorization))
+            {
+                if(!Authenticate(authorization,username,password)){
+                    return Unauthorized();
+                }
+            }
+            else
+            {
+                HttpContext.Response.Headers.Add("WWW-Authenticate", "Basic realm=\"Restricted http methods\"");
+                return Unauthorized();
+            }
+            if(resume == null){
+                return NotFound();
+            }
+            if(resume.Skills.Any(x => x.Name == item.Name)){
+                return Conflict();
+            }
+
+            resume.Skills.Add(item);
+            return Created($"/resume/skills/{item.Name}",null);
+        }
+
+        [HttpPost("languages")]
+        public IActionResult PostLanguage([FromBody] Languages item)
+        {
+            if(HttpContext.Request.Headers.TryGetValue("Authorization", out StringValues authorization))
+            {
+                if(!Authenticate(authorization,username,password)){
+                    return Unauthorized();
+                }
+            }
+            else
+            {
+                HttpContext.Response.Headers.Add("WWW-Authenticate", "Basic realm=\"Restricted http methods\"");
+                return Unauthorized();
+            }
+            if(resume == null){
+                return NotFound();
+            }
+            if(resume.Languages.Any(x => x.Language == item.Language)){
+                return Conflict();
+            }
+
+            resume.Languages.Add(item);
+            return Created($"/resume/languages/{item.Language}",null);
+        }
+        [HttpPost("interests")]
+        public IActionResult PostInterest([FromBody] Interest item)
+        {
+            if(HttpContext.Request.Headers.TryGetValue("Authorization", out StringValues authorization))
+            {
+                if(!Authenticate(authorization,username,password)){
+                    return Unauthorized();
+                }
+            }
+            else
+            {
+                HttpContext.Response.Headers.Add("WWW-Authenticate", "Basic realm=\"Restricted http methods\"");
+                return Unauthorized();
+            }
+            if(resume == null){
+                return NotFound();
+            }
+            if(resume.Interests.Any(x => x.Name == item.Name)){
+                return Conflict();
+            }
+
+            resume.Interests.Add(item);
+            return Created($"/resume/interests/{item.Name}",null);
+        }
+        [HttpPost("references")]
+        public IActionResult PostReference([FromBody] References item)
+        {
+            if(HttpContext.Request.Headers.TryGetValue("Authorization", out StringValues authorization))
+            {
+                if(!Authenticate(authorization,username,password)){
+                    return Unauthorized();
+                }
+            }
+            else
+            {
+                HttpContext.Response.Headers.Add("WWW-Authenticate", "Basic realm=\"Restricted http methods\"");
+                return Unauthorized();
+            }
+            if(resume == null){
+                return NotFound();
+            }
+            if(resume.References.Any(x => x.Name == item.Name)){
+                return Conflict();
+            }
+
+            resume.References.Add(item);
+            return Created($"/resume/references/{item.Name}",null);
+        }
         private bool Authenticate(string authorization, string username, string password)
         {
             var content = authorization.Split(" ");
